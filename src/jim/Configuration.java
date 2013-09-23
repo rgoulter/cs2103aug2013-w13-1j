932169492accddc0c56842ed907f06bb9ad40464
richard <richard@richard-Aspire-V3-471G>
2013-09-23 21:48:20 +0800

    Added a stubbed Configuration.

---
+++ src/jim/Configuration.java
@@ -0,0 +1,18 @@
+package jim;
+
+public class Configuration {
+
+    public Configuration getConfiguration() {
+        // We could maintain a Configuration with a Singleton pattern.
+        // (Search wikipedia. I mean, one static configuration only, and just return that).
+        return null;
+    }
+    
+    public void putValue(String key, String value) {
+        
+    }
+    
+    public String getValue(String key) {
+        return null;
+    }
+}
