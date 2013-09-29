3e61d4a2765572e6e57599fe63c22db67f7821e4
richard <richard@richard-Aspire-V3-471G>
2013-09-29 11:47:57 +0800

    Added stubbed-classes for the Commands we have at present.

---
+++ src/jim/journal/CompleteCommand.java
@@ -0,0 +1,20 @@
+package jim.journal;
+
+public class CompleteCommand extends Command {
+
+    @Override
+    public void execute(JournalManager journalManager) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public String addAnEvent(String anEvent) {
+        return null;
+    }
+
+    @Override
+    public String deleteAnEvent() {
+        return null;
+    }
+
+}
