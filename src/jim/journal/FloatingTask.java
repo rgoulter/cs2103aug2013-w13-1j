69ca47069c71ea2d401ef8b1ea20a9a0eaa26db1
Abel Tan <a0097081@nus.edu.sg>
2013-09-29 18:04:27 +0800

    removed storage in FloatingTask.java removed equals (overiding function)

--- src/jim/journal/FloatingTask.java
+++ src/jim/journal/FloatingTask.java
@@ -1,38 +1,12 @@
 package jim.journal;
 
-import java.util.ArrayList;
-
 public class FloatingTask extends Task {
-	private static final String MESSAGE_PRINT_FLOATING_TASKS = "%d. %s";
-	
-    private static ArrayList <FloatingTask> listOfFloatingTask = new ArrayList <FloatingTask> ();
  
     public FloatingTask(String desc){
         description = desc;
     }
     
-    public static void addFloatingTask (FloatingTask task) {
-    	listOfFloatingTask.add(task);
-    }
-    
     public String getDescription() {
         return description;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        // TODO: An actual equals method
-    	for (int i = 0; i < listOfFloatingTask.size(); i++) {
-    		if (o.equals(listOfFloatingTask.get(i))) {
-    			return true;
-    		}
-    	}
-		return false; 
-    }
-    
-    public void printFloatingTasks() {
-    	for (int i = 0; i < listOfFloatingTask.size(); i++) {
-    		System.out.println(String.format(MESSAGE_PRINT_FLOATING_TASKS,i,listOfFloatingTask.get(i).getDescription()));
-    	}
     }
 }
