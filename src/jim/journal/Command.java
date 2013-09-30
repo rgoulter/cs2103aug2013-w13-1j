9f1f027a90e12415b2b6d07217f47b8722698504
richard <richard@richard-Aspire-V3-471G>
2013-09-30 22:58:43 +0800

    Added an inputLine() method to Command.

--- src/jim/journal/Command.java
+++ src/jim/journal/Command.java
@@ -1,5 +1,9 @@
 package jim.journal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public abstract class Command {
     private StringBuilder outputStringBuilder;
     
@@ -9,6 +13,21 @@ public abstract class Command {
     
     public abstract void execute(JournalManager journalManager);
     
+    // Abstract reading input from command line (should we need it)
+    // so that in UnitTests or in the GUI this can be overridden.
+    protected String inputLine() {
+        try {
+            // Read a line of input from STDIN.
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String         line   = reader.readLine();
+            
+            return line;
+        } catch(IOException ioEx) {
+            ioEx.printStackTrace();
+        }
+        
+        return "";
+    }
     
     protected void output(String outputStr) {
         outputStringBuilder.append(outputStr);
