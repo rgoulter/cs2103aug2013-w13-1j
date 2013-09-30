5b615dd11f90e3d76053e88e9528b712f1779e54
richard <richard@richard-Aspire-V3-471G>
2013-09-30 22:48:29 +0800

    Removed long, discussion comment.

--- src/jim/journal/Command.java
+++ src/jim/journal/Command.java
@@ -7,20 +7,6 @@ public abstract class Command {
         outputStringBuilder = new StringBuilder();
     }
     
-    // execute() does not need to have any arguments (per se).
-    // This implies that each instance of Command refers to a specific command
-    //   e.g. "add lunch Monday"
-    // rather than a family of commands (e.g. new Command(add).executeWithArgs("lunch Monday"))
-    //
-    // execute does, however, have a JournalManager as an argument;
-    // since Command interface connects language processing of SuggestionManager
-    // with Journal logic, we need some way to pass the Journal context to
-    // a generated command.
-    // Thus, inject the dependency here,
-    // OR inject the dependency in the instance of SuggestionManager,
-    // OR have a Singleton of the managers.. (See http://en.wikipedia.org/wiki/Singleton_pattern).
-    //
-    // I chose the first option there.
     public abstract void execute(JournalManager journalManager);
     
     
