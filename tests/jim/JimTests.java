107f36a165f5f119929c13c0e077d1d8df4d1963
richard <richard@richard-Aspire-V3-471G>
2013-09-30 23:51:17 +0800

    Red: Edit Command Execution (with Input). As it should be.

--- tests/jim/JimTests.java
+++ tests/jim/JimTests.java
@@ -83,8 +83,7 @@ public class JimTests {
 
     @Test
     public void testStrictSyntaxEditCommandCanExecute() {
-        // Strict syntax for "add" command:
-        // add <start-date> <start-time> <end-date> <end-time> <words describing event>
+        // Edit Command with the given change-to-task.
 
         // e.g.  "edit MyOldTask to MyNewTask 31/12/13 0000 31/12/13 2359";
 
@@ -106,6 +105,37 @@ public class JimTests {
         
         // This works since, if we have only one Task, then this will be at 0.
         // And if the EditCommand worked, it will replace the task..
+        assertEquals("MyNewTask", journalManager.getAllTasks().get(0).getDescription());
+    }
+
+    @Test
+    public void testStrictSyntaxEditCommandWithInputCanExecute() {
+        // Edit command, getting input from inputLine()
+
+        // e.g.  "edit MyOldTask to MyNewTask 31/12/13 0000 31/12/13 2359";
+
+        Calendar oldStartTime = new GregorianCalendar(2013, 10, 10, 14, 0);
+        Calendar oldEndTime =   new GregorianCalendar(2013, 10, 10, 15, 0);
+        TimedTask myOldTimedTask = new TimedTask(oldStartTime, oldEndTime, "MyOldTask");
+        
+        // n.b. JAN = 0, ..., DEC = 11
+        Calendar newStartTime = new GregorianCalendar(2013, 11, 31, 14, 0);
+        Calendar newEndTime =   new GregorianCalendar(2013, 11, 31, 15, 0);
+        TimedTask expectedNewTimedTask = new TimedTask(newStartTime, newEndTime, "MyNewTask");
+        
+        JournalManager journalManager = new JournalManager(); // Empty; NO TASKS.
+        journalManager.addTask(myOldTimedTask);
+
+        EditCommand editCmd = new EditCommand(Arrays.asList(new Task[]{myOldTimedTask})){
+            protected String inputLine() {
+                return "edit MyOldTask to MyNewTask 31/12/13 0000 31/12/13 2359";
+            }
+        };
+        editCmd.execute(journalManager);
+        
+        // This works since, if we have only one Task, then this will be at 0.
+        // And if the EditCommand worked, it will replace the task..
+        assertNotNull(journalManager.getAllTasks().get(0));
         assertEquals("MyNewTask", journalManager.getAllTasks().get(0).getDescription());
     }
     
