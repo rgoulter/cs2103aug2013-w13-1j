05b22a38a28cc00fa605851dcb88c8221d2b02ce
Qiao Wei <qiaowei94@gmail.com>
2013-10-09 11:20:50 +0800

    The undo command class

---
+++ src/jim/journal/UndoCommand.java
@@ -0,0 +1,14 @@
+package jim.journal;
+
+
+public class UndoCommand extends Command{
+    public UndoCommand(){
+        
+    }
+    @Override
+    public void execute(JournalManager journalManager) {
+        // TODO Auto-generated method stub
+        journalManager.undoLastCommand();
+    }
+
+}
