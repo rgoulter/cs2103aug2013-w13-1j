771f364a16c1b5428a58245e99a2509e124f63c8
richard <richard@richard-Aspire-V3-471G>
2013-10-09 09:19:40 +0800

    RED: Added parse <date> <description> TimedTask test.

--- tests/jim/SuggestionManagerTest.java
+++ tests/jim/SuggestionManagerTest.java
@@ -6,8 +6,10 @@ import jim.journal.Command;
 import jim.journal.FloatingTask;
 import jim.journal.RemoveCommand;
 import jim.journal.Task;
+import jim.journal.TimedTask;
 import jim.suggestions.SuggestionManager;
 
+import org.joda.time.MutableDateTime;
 import org.junit.Test;
 
 
@@ -39,5 +41,30 @@ public class SuggestionManagerTest {
         assertEquals("Parsed description should be the same.",
                      expectedTask.getDescription(),
                      parsedFloatingTask.getDescription());
+    }
+    
+    @Test
+    public void testCanGrammarParseTask_TimedTask_DateDescription () {
+        // For grammar definition: <date> <description>
+        String inputString = "31/12/13 Celebrate New Years Eve";
+        MutableDateTime expectedDate = new MutableDateTime(2013, 12, 31, 0, 0, 0, 0);
+        TimedTask expectedTask = new TimedTask(expectedDate,
+                                               "Celebrate New Years Eve");
+        
+        SuggestionManager suggestionManager = new SuggestionManager();
+        Task parsedTask = suggestionManager.parseTask(inputString.split(" "));
+        
+        assertTrue("Parsed task should be a TimedTask.", parsedTask instanceof TimedTask);
+        
+        TimedTask parsedFloatingTask = (TimedTask) parsedTask;
+        assertEquals("Parsed description should be the same.",
+                     inputString,
+                     parsedFloatingTask.getDescription());
+        assertEquals("Parsed description should be the same.",
+                     expectedTask.getDescription(),
+                     parsedFloatingTask.getDescription());
+        assertEquals("Parsed time should be the same.",
+                     expectedTask.getEndTime(),
+                     parsedFloatingTask.getEndTime());
     }
 }
