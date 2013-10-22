337c239380d260b15165dbac23b522e776f38232
Richard Goulter <devnull@localhost>
2013-10-22 23:21:53 +0800

    Tidied up the Date, Time unit tests so that it's much more readable.

--- tests/jim/ParserDateTimeTests.java
+++ tests/jim/ParserDateTimeTests.java
@@ -10,79 +10,50 @@ import org.joda.time.MutableDateTime;
 import org.junit.Test;
 
 public class ParserDateTimeTests {
+	
+	private void assertDateParsesAs(SuggestionManager suggMan,
+                                    int expectedDayOfMonth,
+                                    int expectedMonth,
+                                    String input) {
+		MutableDateTime dateTime = suggMan.parseDate(input);
+		assertEquals(expectedDayOfMonth, dateTime.getDayOfMonth());
+		assertEquals(expectedMonth, dateTime.getMonthOfYear());
+	}
 
     @Test
     public void testCanParseVariousDateFormats () {
         SuggestionManager suggMan = new SuggestionManager();
-        
-        MutableDateTime dateTime;
-        
-        dateTime = suggMan.parseDate("311213");
-        assertEquals(31, dateTime.getDayOfMonth());
-        assertEquals(12, dateTime.getMonthOfYear());
-        
-        dateTime = suggMan.parseDate("December 31");
-        assertEquals(31, dateTime.getDayOfMonth());
-        assertEquals(12, dateTime.getMonthOfYear());
-        
-        dateTime = suggMan.parseDate("December 31st");
-        assertEquals(31, dateTime.getDayOfMonth());
-        assertEquals(12, dateTime.getMonthOfYear());
-        
-        dateTime = suggMan.parseDate("31 Dec");
-        assertEquals(31, dateTime.getDayOfMonth());
-        assertEquals(12, dateTime.getMonthOfYear());
-        
-        dateTime = suggMan.parseDate("Dec. 31");
-        assertEquals(31, dateTime.getDayOfMonth());
-        assertEquals(12, dateTime.getMonthOfYear());
-        
-        dateTime = suggMan.parseDate("31/12");
-        assertEquals(31, dateTime.getDayOfMonth());
-        assertEquals(12, dateTime.getMonthOfYear());
-        
-        dateTime = suggMan.parseDate("2013/12/31");
-        assertEquals(31, dateTime.getDayOfMonth());
-        assertEquals(12, dateTime.getMonthOfYear());
+
+        assertDateParsesAs(suggMan, 31, 12, "311213");
+        assertDateParsesAs(suggMan, 31, 12, "December 31");
+        assertDateParsesAs(suggMan, 31, 12, "December 31st");
+        assertDateParsesAs(suggMan, 31, 12, "31 Dec");
+        assertDateParsesAs(suggMan, 31, 12, "Dec. 31");
+        assertDateParsesAs(suggMan, 31, 12, "31/12");
+        assertDateParsesAs(suggMan, 31, 12, "2013/12/31");
     }
+
+	private void assertTimeParsesAs(SuggestionManager suggMan,
+                                    int expectedHourOfDay,
+                                    int expectedMinuteOfHour,
+                                    String input) {
+		MutableDateTime dateTime = suggMan.parseTime(input);
+		assertEquals(expectedHourOfDay, dateTime.getHourOfDay());
+		assertEquals(expectedMinuteOfHour, dateTime.getMinuteOfHour());
+	}
+    
 
     @Test
     public void testCanParseVariousTimeFormats () {
         SuggestionManager suggMan = new SuggestionManager();
-        
-        MutableDateTime dateTime;
-        
-        dateTime = suggMan.parseTime("0600");
-        assertEquals(6, dateTime.getHourOfDay());
-        assertEquals(0, dateTime.getMinuteOfHour());
-        
-        dateTime = suggMan.parseTime("0600h");
-        assertEquals(6, dateTime.getHourOfDay());
-        assertEquals(0, dateTime.getMinuteOfHour());
-        
-        dateTime = suggMan.parseTime("6:00");
-        assertEquals(18, dateTime.getHourOfDay());
-        assertEquals(0, dateTime.getMinuteOfHour());
-        
-        dateTime = suggMan.parseTime("06:00");
-        assertEquals(6, dateTime.getHourOfDay());
-        assertEquals(0, dateTime.getMinuteOfHour());
-        
-        dateTime = suggMan.parseTime("6a");
-        assertEquals(6, dateTime.getHourOfDay());
-        assertEquals(0, dateTime.getMinuteOfHour());
-        
-        dateTime = suggMan.parseTime("6P");
-        assertEquals(18, dateTime.getHourOfDay());
-        assertEquals(0, dateTime.getMinuteOfHour());
-        
-        dateTime = suggMan.parseTime("6:30pm");
-        assertEquals(18, dateTime.getHourOfDay());
-        assertEquals(30, dateTime.getMinuteOfHour());
-        
-        
-        dateTime = suggMan.parseTime("6:30 pm");
-        assertEquals(18, dateTime.getHourOfDay());
-        assertEquals(30, dateTime.getMinuteOfHour());
+
+        assertTimeParsesAs(suggMan, 6, 0, "0600");
+        assertTimeParsesAs(suggMan, 6, 0, "0600h");
+        assertTimeParsesAs(suggMan, 18, 0, "6:00");
+        assertTimeParsesAs(suggMan, 6, 0, "06:00");
+        assertTimeParsesAs(suggMan, 6, 0, "6a");
+        assertTimeParsesAs(suggMan, 18, 0, "6P");
+        assertTimeParsesAs(suggMan, 18, 30, "6:30pm");
+        assertTimeParsesAs(suggMan, 18, 30, "6:30 pm");
     }
 }
